a104d7c4075fcfee74e7e9de169678d7078e5f7b
gaboalej <gabobnlk@gmail.com>
2023-06-20 02:05:24 -0300

    creacion de las capas y agregue la consulta del server de sql a la clase NegocioDbContext

---
+++ SolutionCrud/ProyectoCrud.Datos/DatosContext/NegocioDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCrud.Modelos;
+namespace ProyectoCrud.Datos.DatosContext;
+
+public partial class NegocioDbContext : DbContext
+{
+    public NegocioDbContext()
+    {
+    }
+
+    public NegocioDbContext(DbContextOptions<NegocioDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC07E8851C04");
+
+            entity.Property(e => e.Activo).HasColumnName("activo");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreProducto)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("money");
+            entity.Property(e => e.Stock).HasColumnName("stock");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SolutionCrud/ProyectoCrud.Modelos/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProyectoCrud.Datos.DatosContext;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string NombreProducto { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public string Marca { get; set; } = null!;
+
+    public decimal? Precio { get; set; }
+
+    public int? Stock { get; set; }
+
+    public bool? Activo { get; set; }
+}
